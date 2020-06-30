80eda3da05b65a4eac05049d8dcb6d15aa91f63a
lucas <lucascpinheiro10@gmail.com>
2020-06-30 14:43:54 -0300

    start migration to typescript

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-unresolved */
+import express from 'express';
+import cors from 'cors';
+import bookRouter from './routes/index';
+
+const PORT = 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(bookRouter);
+
+app.listen(PORT, () => console.log(`server stared at http://localhost:${PORT}`));
