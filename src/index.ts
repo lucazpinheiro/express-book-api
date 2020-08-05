e10a572c44bc81376886aadd91447d3cf67b57bd
lucas <lucascpinheiro10@gmail.com>
2020-08-05 15:33:02 -0300

    Start refactor

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import bookRouter from './routes/index';
+// import bookRouter from './routes/index';
 
 const PORT = 5000;
 
@@ -8,6 +8,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(bookRouter);
+// app.use(bookRouter);
 
 app.listen(PORT, () => console.log(`server stared at http://localhost:${PORT}`));
