af0ef67d1c7fb3d63e4ed67193e5f7413d47db36
lucas <lucascpinheiro10@gmail.com>
2020-08-07 11:13:44 -0300

    Add mongoose and mongoose types, implement routes skeleton

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,11 +1,34 @@
-export default async function find() {
-  return [{
-    title: 'The Catcher in the Rye',
-    author: 'J. D. Salinger',
-    country: 'US',
-    language: 'English',
-    publicationDate: {
-      year: 1951,
-    },
-  }];
+interface BookPublicationDate {
+  year: number,
+  month?: number,
+}
+
+interface BookInterface {
+  title: string,
+  author: string,
+  country?: string,
+  language?: string,
+  publicationDate: BookPublicationDate,
+}
+
+interface BooksList {
+  [index: number]: BookInterface,
+}
+
+const bookList: BooksList = [{
+  title: 'The catcher in the Rye',
+  author: 'J. D. Salinger',
+  country: 'US',
+  language: 'EN-US',
+  publicationDate: {
+    year: 1951,
+  },
+}];
+
+export async function find() {
+  return bookList;
+}
+
+export async function createBook(bookObj: object) {
+  console.log(bookObj);
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,5 @@
-import { Router, Request, Response } from 'express';
-import find from '../model/index';
+import { Router, Request, Response, NextFunction } from 'express';
+import { find } from '../model/index';
 
 interface BookPublicationDate {
   year: number,
@@ -20,7 +20,23 @@ interface BooksList {
 
 const router = Router();
 
-// get all
+async function getBook(req: Request, res: Response, next: NextFunction) {
+  // let book;
+  // try {
+  //   book = await Books.findById(req.params.id);
+  //   if (book == null) {
+  //     return res.status(404).json({ message: 'Cannot find book' });
+  //   }
+  // } catch (err) {
+  //   return res.status(500).json({ message: err.message });
+  // }
+
+  // res.book = book;
+  // next();
+}
+
+
+// getting all
 router.get('/', async (req: Request, res: Response) => {
   try {
     const books: BooksList = await find();
@@ -28,6 +44,46 @@ router.get('/', async (req: Request, res: Response) => {
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
+});
+
+// gettting by id
+// router.get('/:bookid', getBook, async (req: Request, res: Response) => {
+//   console.log(`get book by id: ${req.params.id}`);
+//   console.log('book data: ', res.book);
+//   res.send(res.book);
+// });
+
+// creating new register
+router.post('/', async (req: Request, res: Response) => {
+  console.log('create new book');
+  console.log('new book data: ', req.body);
+  // const book = new Books({
+  //   title: req.body.title,
+  //   author: req.body.author,
+  //   publicationDate: req.body.publicationDate,
+  // });
+  // try {
+  //   const newBook = await book.save();
+  //   res.status(201).json(newBook);
+  // } catch (err) {
+  //   res.status(400).json({ message: err.message });
+  // }
+});
+
+// router.patch('/:bookid', getBook, async (req, res) => {
+//   console.log(`update book by id: ${req.params.id}`);
+//   console.log('updated book data: ', res.book);
+// });
+
+// deleting by id
+router.delete('/:id', getBook, async (req: Request, res: Response) => {
+  console.log(`delete book by id: ${req.params.id}`);
+  // try {
+  //   await res.book.remove();
+  //   res.json({ message: 'Deleted Book' });
+  // } catch (err) {
+  //   res.status(500).json({ message: err.message });
+  // }
 });
 
 export default router;
