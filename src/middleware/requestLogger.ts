1519e3fbf14b05fdf7e5457bcfaf0279a678fe89
lucazpinheiro <lucascpinheiro10@gmail.com>
2020-10-04 15:34:03 -0300

    Minor change on requestLogger

--- src/middleware/requestLogger.ts
+++ src/middleware/requestLogger.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
+import logger from '../helpers/logger';
 
 export default function requestLogger(req: Request, res: Response, next: NextFunction) {
-  console.log('Request logged:', req.method, req.path);
+  logger(`Request logged: ${req.method} ${req.path}`);
   next();
 }
